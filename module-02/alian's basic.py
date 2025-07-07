9067f029a0b7933d21cab32e8758e38cb8444fd7
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-07 23:57:46 +0600

    Update alian's basic.py

--- module-02/alian's basic.py
+++ module-02/alian's basic.py
@@ -1,39 +1,39 @@
-# print("Welcome new world")
-# #fuel=0.5
-# # home=38880
+print("Welcome new world")
+#fuel=0.5
+# home=38880
 
-# home=int(input("Enter ditance : "))
-# fuel=float(input("Enter fuel :"))
-# need=home*fuel
-# print("Need :",int(need),"unit")
+home=int(input("Enter ditance : "))
+fuel=float(input("Enter fuel :"))
+need=home*fuel
+print("Need :",int(need),"unit")
 
-# chocolate=3
-# sprinkle=2
-# price_choco=5
-# price_sprin=6
-# total=(chocolate*price_choco)+(sprinkle*price_sprin)
-# print("Total Cost : ",total)
+chocolate=3
+sprinkle=2
+price_choco=5
+price_sprin=6
+total=(chocolate*price_choco)+(sprinkle*price_sprin)
+print("Total Cost : ",total)
 
 
-# tax=total*0.1
-# final_cost=total+tax
-# print("Final Cost :",final_cost)
+tax=total*0.1
+final_cost=total+tax
+print("Final Cost :",final_cost)
 
-# total_slices=8
-# aliens=3
+total_slices=8
+aliens=3
 
-# slices_per_alien=total_slices/aliens
-# print("Per alien gets : ",slices_per_alien,"Pices")
+slices_per_alien=total_slices/aliens
+print("Per alien gets : ",slices_per_alien,"Pices")
 
-# full_slices=total_slices // aliens
-# print("Each aliens will get ",full_slices,"Pices of full slices")
-# left=total_slices % aliens
-# print("Left Over slices : ",left)
-# size=[54,234,65,123,65,23]
-# print(min(size))
-# print(max(size))
-# movement= -15
-# distance=abs(movement)
-# print("Distance :",distance)
+full_slices=total_slices // aliens
+print("Each aliens will get ",full_slices,"Pices of full slices")
+left=total_slices % aliens
+print("Left Over slices : ",left)
+size=[54,234,65,123,65,23]
+print(min(size))
+print(max(size))
+movement= -15
+distance=abs(movement)
+print("Distance :",distance)
 power=pow(3,4)
 print(power)
