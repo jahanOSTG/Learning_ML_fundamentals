51b6d001a979517271e7169fa8c37dc3d938b0e1
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-07 23:56:43 +0600

    Add files via upload

---
+++ module-02/loop.py
@@ -0,0 +1,25 @@
+# donuts=["glazed","chocolate","galactic sprinkle","nut"]
+# print(donuts)
+
+# for dount in donuts:
+#     print(dount)
+    
+# angle=0
+# while angle<90:
+#     print("Angle :",angle)
+#     angle=angle+30
+    
+# i=2
+# while i<11:
+#     print(i)
+#     i=i+2
+
+# for i in range(2,11,2):
+#     print(i)
+
+protocols=["ok","ok","bad","ok"]
+
+for i,protocol in enumerate(protocols):
+    print(i,protocol)
+    if protocol == "bad":
+        break
---
+++ module-02/new.py
@@ -0,0 +1,46 @@
+import math
+# distance_to_home=38440
+# gravity=9.8
+
+# thurst_needed=math.sqrt(gravity*distance_to_home)
+# # print(thurst_needed)
+
+
+# height=15
+# base_length=10
+# angle_radius=math.atan(heigh
+# t/base_length)
+# print(angle_radius)
+# angle_degree=math.degrees(angle_radius)
+# print(angle_degree)
+# print("Round angle: ",round(angle_degree))
+# num=34.54345
+# print(math.ceil(num))
+# print(math.floor(num))
+
+# tweet="Hi earth! Sunny here reporting from sector 9"
+# print(tweet[:5])
+# print(tweet[-7:])
+# # print(tweet[6:11])
+# messy="HI##,I ## am J#anie"
+# clean=messy.replace('#','').replace(' ','')
+# print(clean)
+
+# text="faria jahan janie"
+# print(text.upper())
+# print(text.capitalize())
+# print(text.title())
+# print(text.split('a'))
+
+planet = "Eartaha"
+temperature = 22.5678
+weather = "rainy"
+
+report = "planet: {}, Temp: {:.2f}, weather: {}".format(planet, temperature, weather)
+print(report)
+
+
+mass=73.54
+print(f"My mass {mass:.2f} kg")
+print(planet.count('a'))
+
