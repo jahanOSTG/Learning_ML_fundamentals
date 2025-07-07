127fd9ea87f2eeb365da3b57eed2cd349a809fcb
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-07 23:59:03 +0600

    Update loop.py

--- module-02/loop.py
+++ module-02/loop.py
@@ -1,23 +1,23 @@
-# donuts=["glazed","chocolate","galactic sprinkle","nut"]
-# print(donuts)
+ donuts=["glazed","chocolate","galactic sprinkle","nut"]
+ print(donuts)
 
-# for dount in donuts:
-#     print(dount)
+for dount in donuts:
+    print(dount)
     
-# angle=0
-# while angle<90:
-#     print("Angle :",angle)
-#     angle=angle+30
+angle=0
+ while angle<90:
+    print("Angle :",angle)
+    angle=angle+30
     
-# i=2
-# while i<11:
-#     print(i)
-#     i=i+2
+ i=2
+ while i<11:
+    print(i)
+    i=i+2
 
-# for i in range(2,11,2):
-#     print(i)
+for i in range(2,11,2):
+     print(i)
 
-protocols=["ok","ok","bad","ok"]
+ protocols=["ok","ok","bad","ok"]
 
 for i,protocol in enumerate(protocols):
     print(i,protocol)
