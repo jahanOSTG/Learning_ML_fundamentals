2ab639b58287d572d8bbf7447a024884f6a7fa49
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-08 22:51:49 +0600

    Update new.py

--- module-02/new.py
+++ module-02/new.py
@@ -1,36 +1,36 @@
 import math
 distance_to_home=38440
-# gravity=9.8
+gravity=9.8
 
-# thurst_needed=math.sqrt(gravity*distance_to_home)
-# # print(thurst_needed)
+thurst_needed=math.sqrt(gravity*distance_to_home)
+# print(thurst_needed)
 
 
-# height=15
-# base_length=10
-# angle_radius=math.atan(heigh
-# t/base_length)
-# print(angle_radius)
-# angle_degree=math.degrees(angle_radius)
-# print(angle_degree)
-# print("Round angle: ",round(angle_degree))
-# num=34.54345
-# print(math.ceil(num))
-# print(math.floor(num))
+height=15
+base_length=10
+angle_radius=math.atan(heigh
+t/base_length)
+print(angle_radius)
+angle_degree=math.degrees(angle_radius)
+print(angle_degree)
+print("Round angle: ",round(angle_degree))
+num=34.54345
+print(math.ceil(num))
+print(math.floor(num))
 
-# tweet="Hi earth! Sunny here reporting from sector 9"
-# print(tweet[:5])
-# print(tweet[-7:])
-# # print(tweet[6:11])
-# messy="HI##,I ## am J#anie"
-# clean=messy.replace('#','').replace(' ','')
-# print(clean)
+tweet="Hi earth! Sunny here reporting from sector 9"
+print(tweet[:5])
+print(tweet[-7:])
+# print(tweet[6:11])
+messy="HI##,I ## am J#anie"
+clean=messy.replace('#','').replace(' ','')
+print(clean)
 
-# text="faria jahan janie"
-# print(text.upper())
-# print(text.capitalize())
-# print(text.title())
-# print(text.split('a'))
+text="faria jahan janie"
+print(text.upper())
+print(text.capitalize())
+print(text.title())
+print(text.split('a'))
 
 planet = "Eartaha"
 temperature = 22.5678
