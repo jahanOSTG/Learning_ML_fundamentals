e398063f5c18b3e5da40dd19abf1a3ebc2870313
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-11 22:57:45 +0600

    Update list.py

--- Module_02/list.py
+++ Module_02/list.py
@@ -1,37 +1,37 @@
-# # list dictioanr func
-# snacks=["chips","Soda","Cake"]
-# print(snacks)
-# # # After adding cookies 
-# snacks.append("cookies")
-# print(snacks)
+# list dictioanr func
+snacks=["chips","Soda","Cake"]
+print(snacks)
+# # After adding cookies 
+snacks.append("cookies")
+print(snacks)
 
-# snacks.remove("Soda")
-# print(snacks)
+snacks.remove("Soda")
+print(snacks)
 
-# # sort
-# snacks.sort()
-# print(snacks)
+# sort
+snacks.sort()
+print(snacks)
 
-#print("Middle index snacks :",snacks[-2])
-# snacks[1]="juice"
-# print(snacks)
+print("Middle index snacks :",snacks[-2])
+snacks[1]="juice"
+print(snacks)
 
-# snacks.insert(1,"chocolate")
-# print(snacks)
+snacks.insert(1,"chocolate")
+print(snacks)
 
-# del snacks[3]
-# print(snacks)
+del snacks[3]
+print(snacks)
 
 
-# party_bag=[
-#     ["chips","cookies"],
-#     ["cake","Juice"],
-#     ["soda","pizza"]
-# ]
+party_bag=[
+    ["chips","cookies"],
+    ["cake","Juice"],
+    ["soda","pizza"]
+]
 
-# for bag in party_bag:
-#     for item in bag:
-#        print(item)
+for bag in party_bag:
+    for item in bag:
+       print(item)
 table1=["chips","juices"]
 table2=["cake","cookie"]
 print(table1+table2)
