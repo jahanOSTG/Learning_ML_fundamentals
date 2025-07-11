f15c874a024cec5c8f0a6d1215b2186dc6df3b57
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-12 01:45:19 +0600

    Add files via upload

---
+++ Module_02/function.py
@@ -0,0 +1,44 @@
+# def my_function():
+#     print("Hellow form janie")
+    
+# my_function()
+
+# def chowdhury(first_name):
+#     print(first_name+ " Chowdhury")
+    
+# chowdhury("Janie")
+# chowdhury("Antor")
+# chowdhury("Habib")
+
+
+# def concat_func(fname,lname):
+#     print(fname + " "+ lname)
+    
+    
+# concat_func("Janie","Khan")
+
+
+# def concat_func(fname,lname="Khan"):
+#     print(fname + " "+ lname)
+    
+    
+# concat_func("Janie")
+
+# def sum(x,y):
+#     print(x+y)
+# #sum(3,5)
+
+# x=int(input("x :"))
+# y=int(input("y :"))
+# sum(x,y)
+
+
+def fruits(fruit):
+    for x in fruit:
+        print(x)
+fruit_list=["apple","banana"]
+fruits(fruit_list)
+
+def my(x):
+  return 5*x
+print(my(5))
