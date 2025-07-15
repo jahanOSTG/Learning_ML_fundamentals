553034592654d990c959d37b400ff86cca35b59c
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-15 23:20:07 +0600

    Add files via upload

---
+++ Module_02/problem02.py
@@ -0,0 +1,8 @@
+#Write a function to make an hour-to-minute converter.
+def convert_hour_to_minite(h):
+    return h*60
+
+hour=int(input("Enter hour to convert in minite: "))
+print(convert_hour_to_minite(hour))
+
+
