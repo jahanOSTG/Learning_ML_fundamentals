efe9a56e0534b0267a7e261dc603a97f7e365224
Faria Jahan Janie <janiefaria315@gmail.com>
2025-07-22 22:39:06 +0600

    Add files via upload

--- Module-03/marvels.py
+++ Module-03/marvels.py
@@ -1,34 +1,34 @@
-# #class
-# class Avanger:
-#     def fight(self):
-#         print("Avangers start fighting")
-
-#  #object
-# ironman=Avanger() 
-# hulk=Avanger()
-
-# ironman.fight()
-# hulk.fight()
-
-#introdusing method
 #class
-# class Avanger:
-#     def introduce(self,name):
-#         print(f"I am {name} and i protect the world")
+class Avanger:
+    def fight(self):
+        print("Avangers start fighting")
 
-#  #object
-# ironman=Avanger()
-# thor=Avanger()
-# ironman.introduce("Tony")
-# thor.introduce("Thor")
+ #object
+ironman=Avanger() 
+hulk=Avanger()
+
+ironman.fight()
+hulk.fight()
+
+#introducing method
+#class
+class Avanger:
+    def introduce(self,name):
+        print(f"I am {name} and i protect the world")
+
+ #object
+ironman=Avanger()
+thor=Avanger()
+ironman.introduce("Tony")
+thor.introduce("Thor")
 
 
 
 #default constructor
-# class Averger:
-#     def __init__(this):
-#         print("A new avenger join in this team")
-# captain=Averger()
+class Averger:
+    def __init__(this):
+        print("A new avenger join in this team")
+captain=Averger()
 
 
 #parameterized constructor
@@ -39,5 +39,25 @@ class Avanger:
     
     def show(self):
         print(f"{self.name} has power: {self.power}")
-spiderman=Avanger("spiderman","web-sliging")
+        
+input_x=input("Enter name :")
+spiderman=Avanger(input_x,"web-sliging")
 spiderman.show()
+
+#inheritance
+
+class Hero:    #parent class
+    def __init__(self,name):
+            self.name=name
+    
+    
+    def protect(self):
+        print("Protecting the world")
+        
+class Ironman(Hero):  #child class
+    def Fly(self):
+        print(f"{self.name} Flying in the sky")
+        
+tony=Ironman("Ironman")
+tony.protect()
+tony.Fly()
